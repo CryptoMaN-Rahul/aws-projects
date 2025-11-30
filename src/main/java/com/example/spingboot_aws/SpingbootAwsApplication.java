7794e8ffc7720d915066ef4984b407569d08cc9c
CryptoMaN-Rahul <rahulmadwalkar5@gmail.com>
2025-11-30 18:12:06 +0530

    first

---
+++ src/main/java/com/example/spingboot_aws/SpingbootAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.spingboot_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingbootAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spingboot_aws/SpingbootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spingboot_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
